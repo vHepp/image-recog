b93b0a5cdc44198542fcee6e998e94dfae7d608a
vHepp <vuhepola1227@gmail.com>
2022-02-14 10:08:19 -0500

    followed guide to setup mobilenet from @tensorflow-models --> https://www.youtube.com/watch?v=S_Lg1bVbqY4

--- server.js
+++ server.js
@@ -3,6 +3,10 @@ const express = require("express");
 const app = express();
 const port = 5000;
 
+app.use('/', (req,res) => {
+	res.send("It works");
+})
+
 app.listen(port, () => {
 	console.log(`listening on port ${port}`);
 })
