242fa3f34828fd3109f0fb4274a7c32c589a98d9
vHepp <vuhepola1227@gmail.com>
2022-02-14 10:06:54 -0500

    followed guide to setup mobilenet from @tensorflow-models --> https://www.youtube.com/watch?v=S_Lg1bVbqY4

---
+++ client/src/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Tfjs from './Components/Tfjs'
+
+const App = () => {
+  return (
+    <Tfjs />
+  )
+}
+
+export default App
---
+++ client/src/Components/FileUpload.js
@@ -0,0 +1,13 @@
+import React, { useState } from 'react'
+
+
+const FileUpload = () => {
+
+	return (
+		<div>
+			FileUpload
+		</div>
+	)
+}
+
+export default FileUpload
