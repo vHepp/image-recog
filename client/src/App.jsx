a00b6a2bf905df11990338b40bcf46b6e169c558
vHepp <vuhepola1227@gmail.com>
2022-04-28 21:52:10 -0400

    fixed App.jsx import issues

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,11 @@
 import React from 'react'
-import Tesseract from './Components/Tesseract'
 import Tfjs from './Components/Tfjs'
-import Tfjsv2 from './Components/Tfjsv2'
 
 const App = () => {
   return (
-    <>
+    <React.Fragment>
       <Tfjs />
-      {/* <Tfjsv2 /> */}
-      {/* <Tesseract /> */}
-    </>
+    </React.Fragment>
   )
 }
 
